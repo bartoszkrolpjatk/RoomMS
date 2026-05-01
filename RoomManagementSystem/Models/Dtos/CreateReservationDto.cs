7d0600d85dff9f2d41061496db4d42c006f89134
s31572 <s31572@pjwstk.edu.pl>
2026-05-01 07:31:53 +0200

    refactor: not use nullable types

--- RoomManagementSystem/Models/Dtos/CreateReservationDto.cs
+++ RoomManagementSystem/Models/Dtos/CreateReservationDto.cs
@@ -4,11 +4,24 @@ namespace RoomManagementSystem.Models.Dtos;
 
 public record CreateReservationDto
 {
-    [Required] public required long RoomId { get; set; }
-    [Required] public required string OrganizerName { get; set; }
-    [Required] public required string Topic { get; set; }
-    [Required] public required DateOnly Date { get; set; }
-    [Required] public required TimeOnly StartTime { get; set; }
-    [Required] public required TimeOnly EndTime { get; set; }
-    [Required] public required ReservationStatus Status { get; set; }
+    [Required] 
+    public required long RoomId { get; set; }
+    
+    [Required] 
+    public required string OrganizerName { get; set; }
+    
+    [Required] 
+    public required string Topic { get; set; }
+    
+    [Required] 
+    public required DateOnly Date { get; set; }
+    
+    [Required] 
+    public required TimeOnly StartTime { get; set; }
+    
+    [Required] 
+    public required TimeOnly EndTime { get; set; }
+    
+    [Required] 
+    public required ReservationStatus Status { get; set; }
 };
--- RoomManagementSystem/Models/Dtos/UpdateRoomDto.cs
+++ RoomManagementSystem/Models/Dtos/UpdateRoomDto.cs
@@ -2,18 +2,24 @@
 
 namespace RoomManagementSystem.Models.Dtos;
 
-public record UpdateRoomDto(//todo: remove nullable types
+public record UpdateRoomDto
+{
     [Required]
     [StringLength(20, MinimumLength = 1)]
-    string? Name,
-    [Required]
-    BuildingCode? BuildingCode,
-    [Required]
-    int? Floor,
-    [Required]
-    [Range(1, 50)]
-    uint? Capacity,
-    [Required]
-    bool? HasProjector,
-    [Required]
-    bool? IsActive);
+    public required string Name { get; set; }
+
+    [Required] 
+    public required BuildingCode BuildingCode { get; set; }
+    
+    [Required] 
+    public required int Floor { get; set; }
+    
+    [Required] [Range(1, 50)] 
+    public required uint Capacity { get; set; }
+    
+    [Required] 
+    public required bool HasProjector { get; set; }
+    
+    [Required] 
+    public required bool IsActive { get; set; }
+}
